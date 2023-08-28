7a83c8017de0e87de9d38aee883f4b9a691be04b
sushant <prashantpisal77@gmail.com>
2023-08-28 13:59:53 +0530

    [PisalSushant]This is my second push

--- src/test/java/E_CommerceWebSites/Sample_1.java
+++ src/test/java/E_CommerceWebSites/Sample_1.java
@@ -7,14 +7,22 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class Sample_1 {
+	static WebDriver driver;
   @Test
   public void openAmazon() {
 	   Reporter.log("Open Amazon", true);
 	   ChromeOptions opt =new ChromeOptions();
   	   opt.addArguments("--remote-allow-origins=*");
 	   System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-	   WebDriver driver=new ChromeDriver(opt);
+	    driver=new ChromeDriver(opt);
 	   driver.get("https://www.amazon.in/");
+	   
+	   Reporter.log("Open Flipkart", true);
+	   ChromeOptions op =new ChromeOptions();
+  	   opt.addArguments("--remote-allow-origins=*");
+	   System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+	   driver=new ChromeDriver(op);
+	   driver.get("https://www.flipkart.in/");
 	   
   }
 }
